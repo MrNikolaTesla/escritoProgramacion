5ae0c810c6b872267a95d41cc04b06915606be81
Lautaro <110629515+MrNikolaTesla@users.noreply.github.com>
2022-11-25 13:02:21 -0300

    Update Estudiantes_view.php

--- vista/Estudiantes_view.php
+++ vista/Estudiantes_view.php
@@ -40,12 +40,34 @@
                 }
             });
         });
+<<<<<<< HEAD
         // Confirmación de eliminar usuario, funciona cada vez que se quiere eliminar un usuario
         function asegurar() {
             rc = confirm("¿Seguro que desea Eliminar al usuario?");
             return rc;
         }
     </script>
+=======
+        // Confirmación de eliminar usuario
+        function asegurar() {
+            rc = confirm("¿Seguro que desea Eliminar al estudiante?");
+            return rc;
+        }
+
+        //ESTO https://www.javatpoint.com/sweetalert
+        sweetAlert({
+                title: "Are you sure?",
+                text: "You will not be able to recover this imaginary file!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!"
+            },
+            deleteIt()
+        );
+    </script>
+
+>>>>>>> parent of 38637fd (messi)
 </head>
 
 <body>
