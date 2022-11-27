c06cb214fc8edc18235ff6af2a37b346c1a47be3
Lautaro <110629515+MrNikolaTesla@users.noreply.github.com>
2022-11-26 23:28:05 -0300

    xd

--- pagina/RegistroEstudiantes.php
+++ pagina/RegistroEstudiantes.php
@@ -22,63 +22,93 @@
 
 <body>
         <form method="POST">
-        <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
-                <div class="container">
-                        <a class="navbar-brand">Ingresar Estudiante</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                        </button>
+                <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+                        <div class="container">
+                                <a class="navbar-brand">Ingresar Estudiante</a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                </button>
 
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav ml-auto">
-                                        <li class="nav-item">
-                                                <a class="nav-link" href="../index.php">Listado de estudiantes</a>
-                                        </li>
-                                </ul>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                        <ul class="navbar-nav ml-auto">
+                                                <li class="nav-item">
+                                                        <a class="nav-link" href="../index.php">Listado de estudiantes</a>
+                                                </li>
+                                        </ul>
 
-                        </div>
-                </div>
-        </nav>
-
-        <main class="my-form">
-        <div class="cotainer">
-        <div class="row justify-content-center">
-                <div class="col-md-8">
-                <div class="card">
-                        <div class="card-header">Datos del estudiante</div>
-                        <?php require_once("../controlador/ControladorRegistroEstudiante.php"); ?>
-                        <div class="card-body">
-                                                <div class="form-group row">
-                                                <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombres y apellidos</label>
-                                                        <div class="col-md-6">
-                                                        <input type="text" id="nombre" class="form-control" name="nombre" maxlength="50"></div>
-                                                        </div>
-
-                                                <div class="form-group row">
-                                                <label for="grupo" class="col-md-4 col-form-label text-md-right">Grupo</label>
-                                                        <div class="col-md-6">
-                                                        <input type="text" id="grupo" class="form-control" name="grupo" maxlength="10"></div>
-                                                </div>
-
-                                                <div class="form-group row">
-                                                <label for="notas" class="col-md-4 col-form-label text-md-right">Notas</label>
-                                                        <div class="col-md-6">
-                                                        <input type="text" id="notas" class="form-control" name="notas" maxlength="5"></div>
-                                                </div>
-
-                                                <div class="col-md-6 offset-md-4">
-                                                <button type="submit" class="btn btn-primary" name="boton_registro" value="enviar">Registrar estudiante</button></div>
-                                                </div>
-                                                
-                                        </form>
                                 </div>
                         </div>
-                </div>
-        </div>
-        </div>
+                </nav>
 
-        </main>
+                <main class="my-form">
+                        <div class="cotainer">
+                                <div class="row justify-content-center">
+                                        <div class="col-md-8">
+                                                <div class="card">
+                                                        <div class="card-header">Datos del estudiante</div>
+                                                        <?php require_once("../controlador/ControladorRegistroEstudiante.php"); ?>
+                                                        <div class="card-body">
+                                                                <div class="form-group row">
+                                                                        <label for="fecha" class="col-md-4 col-form-label text-md-right">Fecha</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="datetime-local" id="fecha" class="form-control" name="fecha" required>
+                                                                        </div>
+                                                                </div>
 
+                                                                <div class="form-group row">
+                                                                        <label for="centro" class="col-md-4 col-form-label text-md-right">Centro</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="text" id="centro" class="form-control" name="centro" required>
+                                                                        </div>
+                                                                </div>
+
+                                                                <div class="form-group row">
+                                                                        <label for="cedula" class="col-md-4 col-form-label text-md-right">Documento</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="text" id="cedula" class="form-control" name="cedula" required>
+                                                                        </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                        <label for="nombre_apellido" class="col-md-4 col-form-label text-md-right">Nombre y Apellido</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="text" id="nombre_apellido" class="form-control" name="nombre_apellido" required>
+                                                                        </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                        <label for="curso" class="col-md-4 col-form-label text-md-right">Curso</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="text" id="curso" class="form-control" name="curso" required>
+                                                                        </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                        <label for="edad" class="col-md-4 col-form-label text-md-right">Edad</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="text" id="edad" class="form-control" name="edad" required>
+                                                                        </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                        <label for="numero_cel" class="col-md-4 col-form-label text-md-right">Celular</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="text" id="numero_cel" class="form-control" name="numero_cel" required>
+                                                                        </div>
+                                                                </div>
+                                                                <div class="form-group row">
+                                                                        <label for="correo" class="col-md-4 col-form-label text-md-right">Correo</label>
+                                                                        <div class="col-md-6">
+                                                                                <input type="email" id="correo" class="form-control" name="correo" required>
+                                                                        </div>
+                                                                </div>
+                                                                <div class="col-md-6 offset-md-4">
+                                                                        <button type="submit" class="btn btn-primary" name="boton_registro" value="enviar">Registrar estudiante</button>
+                                                                </div>
+
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </main>
+        </form>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
